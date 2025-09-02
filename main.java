5da98db6f5e5b846b6ed75746bcac301bcc6c00f
tommy-Samy123 <samarthiyer9@gmail.com>
2025-09-02 10:39:26 -0700

    Added .gitignore and Subtraction functionality
    Signed-off-by: tommy-Samy123 <samarthiyer9@gmail.com>

--- main.java
+++ main.java
@@ -1,16 +1,30 @@
 import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        String expression = s.nextLine();
+        System.out.println("Result: " + calculator(s.nextLine()));
+    }
+    public static int calculator(String str) {
         int result = 0;
+        int sign = 1;
 
-        for (char c : expression.toCharArray()) {
-            if (Character.isDigit(c)) {
-                result += Character.getNumericValue(c);
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '+') {
+                sign = 1;
+            } else if (c == '-') {
+                sign = -1;
+            } else if (c >= '0' && c <= '9') {
+                StringBuilder num = new StringBuilder();
+                num.append(c);
+                while (i + 1 < str.length() && str.charAt(i + 1) >= '0' && str.charAt(i + 1) <= '9') {
+                    num.append(str.charAt(i + 1));
+                    i++;
+                }
+                result += sign * Integer.parseInt(num.toString());
             }
-            // If it's a '+', we simply continue the loop
         }
-        System.out.println("Result: " + result);
+        return result;
     }
 }
