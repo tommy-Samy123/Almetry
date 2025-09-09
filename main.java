66eb9fc8a895d6a10f57e35835c17a0f8ded5c1d
tommy-Samy123 <samarthiyer9@gmail.com>
2025-09-09 10:11:18 -0700

    Added a try catch to alert for any division by 0 and other errors in input/calculation
    Signed-off-by: tommy-Samy123 <samarthiyer9@gmail.com>

--- main.java
+++ main.java
@@ -17,56 +17,61 @@ public class main {
         str += '+';
 
         for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
+            try {
+                char c = str.charAt(i);
 
-            if (Character.isDigit(c)) {
-                num = num * 10 + (c - '0');
-            }
-
-            if (c == '(') {
-                stack.push((int) sign);
-                stack.push(Integer.MIN_VALUE);  
-                sign = '+';
-                num = 0;
-            } 
-
-            else if (c == ')') {
-                if (sign == '+') stack.push(num);
-                else if (sign == '-') stack.push(-num);
-                else if (sign == '*') stack.push(stack.pop() * num);
-                else if (sign == '/') stack.push(stack.pop() / num);
-                
-                num = 0;
-
-                int sum = 0;
-                int item = stack.pop();
-                while (item != Integer.MIN_VALUE) {
-                    sum += item;
-                    item = stack.pop();
+                if (Character.isDigit(c)) {
+                    num = num * 10 + (c - '0');
                 }
- 
-                sign = (char) stack.pop().intValue();
-                num = sum;
-            }
 
-            else if ("+-*/".indexOf(c) != -1 || i == str.length() - 1) {
-                if (sign == '+') {
-                    stack.push(num);
-                } else if (sign == '-') {
-                    stack.push(-num);
-                } else if (sign == '*') {
-                    stack.push(stack.pop() * num);
-                } else if (sign == '/') {
-                    stack.push(stack.pop() / num);
-                } else {
-                    System.out.println("Something went wrong.");
-                    break;
+                if (c == '(') {
+                    stack.push((int) sign);
+                    stack.push(Integer.MIN_VALUE);  
+                    sign = '+';
+                    num = 0;
+                } 
+
+                else if (c == ')') {
+                    if (sign == '+') stack.push(num);
+                    else if (sign == '-') stack.push(-num);
+                    else if (sign == '*') stack.push(stack.pop() * num);
+                    else if (sign == '/') stack.push(stack.pop() / num);
+                    
+                    num = 0;
+
+                    int sum = 0;
+                    int item = stack.pop();
+                    while (item != Integer.MIN_VALUE) {
+                        sum += item;
+                        item = stack.pop();
+                    }
+     
+                    sign = (char) stack.pop().intValue();
+                    num = sum;
                 }
-                num = 0;
-                sign = c;
-                
-            }
 
+                else if ("+-*/".indexOf(c) != -1 || i == str.length() - 1) {
+                    if (sign == '+') {
+                        stack.push(num);
+                    } else if (sign == '-') {
+                        stack.push(-num);
+                    } else if (sign == '*') {
+                        stack.push(stack.pop() * num);
+                    } else if (sign == '/') {
+                        stack.push(stack.pop() / num);
+                    } else {
+                        System.out.println("Something went wrong.");
+                        break;
+                    }
+                    num = 0;
+                    sign = c;
+                    
+                }
+            }
+            catch(Exception e) {
+                System.out.println("There was an error");
+                return 0;
+            }
         }
         return stack.stream().mapToInt(Integer::intValue).sum();
     }
